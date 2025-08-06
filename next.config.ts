ba780c7b4891c712751ce27b09958fb587d74d83
Sanket <info@byteory.com>
2025-08-06 19:21:40 +0530

    Add 301 redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://tokenry.tools/binance-token-creator',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
